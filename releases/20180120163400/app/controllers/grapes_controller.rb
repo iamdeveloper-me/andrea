5de70be9f9014fefa8bfa2a386c937ce0ae7ffc4
iamdeveloper-me <iamdeveloper.me@gmail.com>
2018-06-04 06:04:09 +0000

    Added 20180120163400 release.

---
+++ releases/20180120163400/app/controllers/grapes_controller.rb
@@ -0,0 +1,11 @@
+# grape controller
+class GrapesController < InheritedResources::Base
+
+   def index
+    @grapes = Grape.where(publish: '1')
+   end
+   def week
+    @grapes = Grape.where(publish: '1',week_pub: '1')
+   end
+end
+
